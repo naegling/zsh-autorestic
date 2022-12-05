aa537ca226bdc3f71324f9d15ac4064b2a14e147
Richard Rutledge <richard@therutledge.com>
2022-12-05 17:35:47 -0500

    restored lazy generation

--- zsh-autorestic.plugin.zsh
+++ zsh-autorestic.plugin.zsh
@@ -6,8 +6,9 @@ fi
 # If the completion file doesn't exist yet, we need to autoload it and
 # bind it to `restic`. Otherwise, compinit will have already done that.
 if [[ ! -f "$ZSH_CACHE_DIR/completions/_autorestic" ]]; then
-  autorestic completion zsh > "$ZSH_CACHE_DIR/completions/_autorestic" &|
   typeset -g -A _comps
   autoload -Uz _autorestic
   _comps[autorestic]=_autorestic
 fi
+
+autorestic completion zsh > "$ZSH_CACHE_DIR/completions/_autorestic" &|
